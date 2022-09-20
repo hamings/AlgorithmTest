0dac2eff96ec3f7c9ade906c0a64d85596ea557a
hamings <wjdgpal9630@gmail.com>
2022-09-20 23:44:12 +0900

    Use for&if statements

---
+++ .idea/src/loop/ReceiptTest.java
@@ -0,0 +1,25 @@
+package loop;
+
+import java.util.Scanner;
+
+public class ReceiptTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int total = sc.nextInt();
+        int type = sc.nextInt();
+        int sum = 0;
+        for(int i = 0; i<type; i++){
+            int price = sc.nextInt();
+            int count = sc.nextInt();
+            sum += price*count;
+
+        }
+        if(total==sum){
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+
+    }
+}
