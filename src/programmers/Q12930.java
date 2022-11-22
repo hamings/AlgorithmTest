28df5ba9756a382d12a6736a63f5a789ecae79aa
hamings <wjdgpal9630@gmail.com>
2022-11-22 13:57:48 +0900

    add Q12930

---
+++ src/programmers/Q12930.java
@@ -0,0 +1,21 @@
+package programmers;
+
+public class Q12930 {
+    public static class Solution {
+        public String solution(String s) {
+            String answer = "";
+            String[] str = s.split("");
+            int index= 0;
+            for(int i = 0;i<str.length;i++){
+                if(!str[i].equals(" ")) {
+                    str[i] = index % 2 == 0 ? str[i].toUpperCase() : str[i].toLowerCase();
+                    index++;
+                }else{
+                    index=0;
+                }
+                answer+=str[i];
+            }
+            return answer;
+        }
+    }
+}
