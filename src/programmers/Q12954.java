39aab4a5b035401ea25ddfed3cb06a84425d8d9a
hamings <wjdgpal9630@gmail.com>
2022-11-25 13:50:57 +0900

    add Q12954

---
+++ src/programmers/Q12954.java
@@ -0,0 +1,13 @@
+package programmers;
+
+public class Q12954 { // x만큼 간격이 있는 n개의 숫자
+    public static class Solution {
+        public long[] solution(int x, int n) {
+            long[] answer = new long[n];
+            for(int i=0;i<n;i++){
+                answer[i]=(long)x*(i+1);
+            }
+            return answer;
+        }
+    }
+}
