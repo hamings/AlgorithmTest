04e475a56c1fed033ac6cd24b111392d77e71208
hamings <wjdgpal9630@gmail.com>
2022-12-06 19:29:15 +0900

    add Q42842

---
+++ src/programmers/Q42842.java
@@ -0,0 +1,20 @@
+package programmers;
+
+public class Q42842 { // 카펫
+    public static class Solution {
+        public int[] solution(int brown, int yellow) {
+            int[] answer = new int[2];
+            for(int i=1;i<=yellow;i++){
+                if(yellow%i==0){
+                    if(((yellow/i)+i)*2+4==brown){
+                        answer[0]=yellow/i+2;
+                        answer[1]=i+2;
+                        break;
+                    }
+                }
+            }
+
+            return answer;
+        }
+    }
+}
