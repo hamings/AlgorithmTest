d01ba0b5024801b8be28b61dcfea1841c5650966
hamings <113500975+hamings@users.noreply.github.com>
2022-12-27 14:20:21 +0900

    [Bronze V] Title: A/B, Time: 216 ms, Memory: 17980 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main { // A/B
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        System.out.println(a/b);
+    }
+}
