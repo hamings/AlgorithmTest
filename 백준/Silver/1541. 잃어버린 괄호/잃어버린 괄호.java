ffbdc5396c07bbb50476ff51b1e4492a1fccaac0
hamings <113500975+hamings@users.noreply.github.com>
2023-02-26 16:45:59 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 124 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st1 = new StringTokenizer(br.readLine(),"-");
+        StringTokenizer st2 = new StringTokenizer(st1.nextToken(),"+");
+
+        int result=0;
+        while(st2.hasMoreTokens()){
+            result+=Integer.parseInt(st2.nextToken());
+        }
+        while(st1.hasMoreTokens()){
+            st2 = new StringTokenizer(st1.nextToken(),"+");
+            while(st2.hasMoreTokens()){
+                result-=Integer.parseInt(st2.nextToken());
+            }
+        }
+        System.out.println(result);
+    }
+}
