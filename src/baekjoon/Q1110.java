21bbbe3a60e3a4186ce6d88324365868f4e55ae8
hamings <wjdgpal9630@gmail.com>
2023-03-21 13:04:49 +0900

    add Q1110

--- src/baekjoon/Q1110.java
+++ src/baekjoon/Q1110.java
@@ -1,11 +1,13 @@
 package baekjoon;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Q1110 { // 더하기 사이클
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         int a = n%10;
         int b = n/10 + a;
         int count = 1;
