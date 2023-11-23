345a1419e461a2140a478181e11bdd492500f8df
hamings <113500975+hamings@users.noreply.github.com>
2023-11-23 15:50:12 +0900

    [level 2] Title: 주식가격, Time: 21.82 ms, Memory: 61.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0;i<prices.length;i++){
+            while(!stack.empty()&&prices[i]<prices[stack.peek()]){
+                int index = stack.pop();
+                answer[index]=i-index;
+            }
+            stack.push(i);   
+        }
+        while(!stack.empty()){
+            int index = stack.pop();
+            answer[index]=prices.length-index-1;
+        }
+        return answer;
+    }
+}
